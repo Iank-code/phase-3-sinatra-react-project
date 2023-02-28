de8b557af1ed5d42cc6607e857d74b96e9775eb9
ianK-code <kian99564@gmail.com>
2023-02-28 15:12:40 +0300

    Added a file for deleting contents excluding the tables and a few modification

--- db/migrate/20230227124330_create_todos.rb
+++ db/migrate/20230227124330_create_todos.rb
@@ -1,11 +1,26 @@
 class CreateTodos < ActiveRecord::Migration[6.1]
-  def change
-    create_table :todos do |t|
-      t.string :name
-      t.string :description
-      t.references :category, null: false, foreign_key: true
+  # def change
+  #   create_table :todos do |t|
+  #     t.string :name
+  #     t.string :description
+  #     t.references :category, null: false, foreign_key: true
 
-      t.timestamps
+  #     t.timestamps
+  #   end
+  # end
+  def up
+    unless ActiveRecord::Base.connection.table_exists?(:todos)
+      create_table :todos do |t|
+        t.string :name
+        t.string :description
+        t.references :category, null: false, foreign_key: true
+
+        t.timestamps
+      end
     end
+  end
+
+  def down
+    drop_table :todos, if_exists: true
   end
 end
--- db/migrate/20230227172219_create_categories.rb
+++ db/migrate/20230227172219_create_categories.rb
@@ -1,8 +1,15 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
-  def change
-    create_table :categories do |t|
-      t.string :category
-      t.timestamps
+  def up
+    unless ActiveRecord::Base.connection.table_exists?(:categories)
+      create_table :categories do |t|
+        t.string :category
+
+        t.timestamps
+      end
     end
+  end
+
+  def down
+    drop_table :categories, if_exists: true
   end
 end
