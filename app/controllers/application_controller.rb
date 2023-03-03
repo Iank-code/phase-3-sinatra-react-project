983d596cfd74c3cacce0d0415d58d9599a95d823
ianK-code <kian99564@gmail.com>
2023-03-03 10:47:21 +0300

    Allow cross origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,66 @@
+# class ApplicationController < Sinatra::Base
+#   set :default_content_type, 'application/json'
+  
+#   # Add your routes here
+#   get "/" do
+#     { message: "Good luck with your project!" }.to_json
+#   end
+#   configure do
+#     enable :cross_origin
+#   end
+
+#   get "/all" do
+#     Todo.all.to_json
+#   end
+
+#   get '/all/:id' do
+#     task = Todo.find(params[:id])
+#     task.to_json
+#   end
+  
+#   patch '/patch/:id' do
+#     review = Todo.find(params[:id])
+#     review.update(
+#       name: params[:name],
+#       description: params[:description]
+#     )
+#     review.to_json
+#   end
+
+#   post '/post' do
+#     cat = Category.create(category: params[:category])
+#     todo = Todo.create(
+#       name: params[:name],
+#       description: params[:description],
+#       category_id: cat.id
+#     )
+#     content_type :json
+#     { category: cat, todo: todo }.to_json
+#   end
+
+#   post '/register' do
+#     users = User.create(
+#       name: params[:name],
+#       email: params[:email],
+#       password: params[:password],
+#       phone_number: params[:phone_number]
+#     )
+#     users.to_json
+#   end
+#   delete '/delete/:id' do
+#     # find the review using the ID
+#     review = Todo.find(params[:id])
+#     # delete the review
+#     review.destroy
+#     # send a response with the deleted review as JSON
+#     review.to_json
+#   end
+
+#   get '/credentials' do
+#     User.all.to_json
+#   end
+# end
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -5,8 +68,20 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  
   configure do
     enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  options "*" do
+    response.headers["Allow"] = "GET, POST, PATCH, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
   end
 
   get "/all" do
@@ -47,6 +122,7 @@ class ApplicationController < Sinatra::Base
     )
     users.to_json
   end
+
   delete '/delete/:id' do
     # find the review using the ID
     review = Todo.find(params[:id])
