72c0b16eb43efb712d3f7a22a1c47b032f1efc1c
ianK-code <kian99564@gmail.com>
2023-03-03 12:54:58 +0300

    Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,39 @@
+# class ApplicationController < Sinatra::Base
+  
+
+
+#   # post '/register' do
+#   #   "This is register part"
+#   #   user_data = JSON.parse(request.body.read)
+#   #   name = user_data['name']
+#   #   email = user_data['email']
+#   #   password = user_data['password']
+#   #   phone_number = user_data['tel']
+#   #   # password = user_data['password']
+
+#   #   # Encrypt the password using bcrypt
+#   #   password_digest = BCrypt::Password.create(password)
+
+#   #   # Create a new User record in the database
+#   #   user = User.create(
+#   #     name: name,
+#   #     email: email,
+#   #     phone_number: phone_number,
+#   #     password_digest: password_digest
+#   #   )
+
+#   #   # Return the user data as a JSON response
+#   #   content_type :json
+#   #   { id: user.id, name: user.name, email: user.email, phone_number: user.phone_number, password_digest: user.password_digest  }.to_json
+#   # end
+
+  
+
+  
+# end
+
+
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -5,14 +41,57 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
-  configure do
-    enable :cross_origin
-  end
 
   get "/all" do
     Todo.all.to_json
   end
 
+  # @api: Enable CORS Headers
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Origin"] = 'http://localhost:3000'
+    # response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
+
+  # @api: Format the json response
+  def json_response(code: 200, data: nil)
+    status = [200, 201].include?(code) ? "SUCCESS" : "FAILED"
+    headers['Content-Type'] = 'application/json'
+    if data
+      [ code, { data: data, message: status }.to_json ]
+    end
+  end
+
+  # @api: Format all common JSON error responses
+  def error_response(code, e)
+    json_response(code: code, data: { error: e.message })
+  end
+
+  # @views: Format the erb responses
+  def erb_response(file)
+    headers['Content-Type'] = 'text/html'
+    erb file
+  end
+
+  # @helper: not found error formatter
+  def not_found_response
+    json_response(code: 404, data: { error: "You seem lost. That route does not exist." })
+  end
+
+  # @api: 404 handler
+  not_found do
+    not_found_response
+  end
   get '/all/:id' do
     task = Todo.find(params[:id])
     task.to_json
