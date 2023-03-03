04151056f616a245c87bd7c1318853b578f04ee9
ianK-code <kian99564@gmail.com>
2023-03-03 10:17:26 +0300

    Fixed a bug in the users table

--- db/migrate/20230228130218_create_users.rb
+++ db/migrate/20230228130218_create_users.rb
@@ -1,12 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.integer :phone_number
-      t.string :password
+  def up
+    unless ActiveRecord::Base.connection.table_exists?(:users)
+      create_table :users do |t|
+        t.string :name
+        t.string :email
+        t.integer :phone_number
+        t.string :password
 
-      t.timestamps
+        t.timestamps
+      end
     end
+  end
+
+  def down
+    drop_table :users, if_exists: true
   end
 end
