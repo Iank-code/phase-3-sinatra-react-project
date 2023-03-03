6e6832d22b6b01f3ba7a8ecce753295e967ada47
ianK-code <kian99564@gmail.com>
2023-03-03 10:27:34 +0300

    Cross origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,45 +1,12 @@
-# class ApplicationController < Sinatra::Base
-  
-
-
-#   # post '/register' do
-#   #   "This is register part"
-#   #   user_data = JSON.parse(request.body.read)
-#   #   name = user_data['name']
-#   #   email = user_data['email']
-#   #   password = user_data['password']
-#   #   phone_number = user_data['tel']
-#   #   # password = user_data['password']
-
-#   #   # Encrypt the password using bcrypt
-#   #   password_digest = BCrypt::Password.create(password)
-
-#   #   # Create a new User record in the database
-#   #   user = User.create(
-#   #     name: name,
-#   #     email: email,
-#   #     phone_number: phone_number,
-#   #     password_digest: password_digest
-#   #   )
-
-#   #   # Return the user data as a JSON response
-#   #   content_type :json
-#   #   { id: user.id, name: user.name, email: user.email, phone_number: user.phone_number, password_digest: user.password_digest  }.to_json
-#   # end
-
-  
-
-  
-# end
-
-
-
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
+  end
+  configure do
+    enable :cross_origin
   end
 
   get "/all" do
